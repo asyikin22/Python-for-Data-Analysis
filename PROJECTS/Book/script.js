69e22216ce2f79d7e3eb750750f62759463b1af8
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-11-04 18:47:45 +0800

    added goodreads & google search function, added navbar

---
+++ PROJECTS/Book/script.js
@@ -0,0 +1,56 @@
+let books = [];
+
+//load books data from JSON file 
+fetch("books.json")
+    .then(response => response.json())
+    .then(data => {
+        books = data;
+
+        //initialize fuse.js wiht books data and search options to allow fuzzy search
+        fuse = new Fuse(books, {
+            keys: ['Title', 'Author'],
+            threshold: 0.4,
+        });
+    })
+    .catch(error => console.error("Error Loading Books data:", error))
+
+function searchBooks() {
+    const query = document.getElementById("search-input").value.toLowerCase();
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = "";
+
+    //perform fuzzy search
+    const results = fuse.search(query)
+
+    if (results.length === 0) {
+        resultsDiv.innerHTML = "<p>No books found. </p>";
+    } else {
+        results.forEach(result => {
+            const book = result.item;
+            const bookDiv = document.createElement("div");
+
+            //Add Goodreads and Google Books links
+            const GoogleBooksURL = `https://www.google.com/search?q=${encodeURIComponent(book.Title + " " + book.Author)}`;
+            bookDiv.innerHTML = `
+                <h4 style="text-align: center;">${book.Title} by ${book.Author}</h4>
+                <div style="text-align: center;">
+                    <a href="${book.URL.trim()}" target="_blank" style="text-decoration: none; color: blue; transition: color 0.3s ease;">Goodreads Link</a><br>
+                    <a href="${GoogleBooksURL}" target="_blank" style="text-decoration: none; color: blue; transition: color 0.3s ease;">Google Search Link</a>
+                </div>
+            `;
+            resultsDiv.appendChild(bookDiv)
+        })
+    }
+}
+//event listener for 'enter' key press
+document.getElementById("search-input").addEventListener("keypress", function(event) {
+    if(event.key === "Enter") {
+        searchBooks();
+    }
+})
+
+//function to clear input() 
+function clearInput() {
+    document.getElementById("search-input").value = "";
+    document.getElementById("results").innerHTML = "";
+}
