bcd22d42c433fb408581b93992fc73cd3ff084af
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-12-05 19:24:49 +0800

    customize css, add search placeholder + footer, add VADER insights

--- PROJECTS/Book/script.js
+++ PROJECTS/Book/script.js
@@ -14,6 +14,26 @@ fetch("books.json")
     })
     .catch(error => console.error("Error Loading Books data:", error))
 
+function displayGif() {
+    const resultsDiv = document.getElementById("results");
+
+    resultsDiv.innerHTML = `
+        <div style="text-align: center; color: gray;">
+            <div style="text-align: center; color: gray; display: flex; justify-content: center; align-items: center; height: 23vh;">
+                <iframe src="https://giphy.com/embed/JIX9t2j0ZTN9S" 
+                        style="max-width: 100%; max-height: 100%; border: none;" 
+                        allowFullScreen>
+                </iframe>
+            </div>
+            <p style="color: brown; font-size: 20px;">No results to display. Start your search!</p>
+        </div>
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    displayGif();
+})
+
 function searchBooks() {
     const query = document.getElementById("search-input").value.toLowerCase();
     const resultsDiv = document.getElementById("results");
@@ -32,10 +52,10 @@ function searchBooks() {
             //Add Goodreads and Google Books links
             const GoogleBooksURL = `https://www.google.com/search?q=${encodeURIComponent(book.Title + " " + book.Author)}`;
             bookDiv.innerHTML = `
-                <h2 style="text-align: center; color: yellowgreen;">${book.Title} by ${book.Author}</h4>
+                <h2 style="text-align: center; color: brown;">${book.Title} by ${book.Author}</h4>
                 <div style="text-align: center;">
-                    <a href="${book.URL.trim()}" target="_blank" style="text-decoration: none; color: pink; transition: color 0.3s ease;">Goodreads Link</a><br>
-                    <a href="${GoogleBooksURL}" target="_blank" style="text-decoration: none; color: violet; transition: color 0.3s ease;">Google Search Link</a>
+                    <a href="${book.URL.trim()}" target="_blank" style="text-decoration: none; color: black; transition: color 0.3s ease;">Goodreads</a><br>
+                    <a href="${GoogleBooksURL}" target="_blank" style="text-decoration: none; color: black; transition: color 0.3s ease;">Google</a>
                 </div>
             `;
             resultsDiv.appendChild(bookDiv)
@@ -52,5 +72,8 @@ document.getElementById("search-input").addEventListener("keypress", function(ev
 //function to clear input() 
 function clearInput() {
     document.getElementById("search-input").value = "";
-    document.getElementById("results").innerHTML = "";
+    
+    displayGif();
+
+    
 }
